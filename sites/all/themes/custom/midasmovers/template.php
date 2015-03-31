f8309a4d5aae7d20e785cebac7b1568f948356b6
jason kühn <spaceyraygun@gmail.com>
2015-03-30 20:19:24 -0400

    update bootstrap. some styles. updated feature.

--- sites/all/themes/custom/midasmovers/template.php
+++ sites/all/themes/custom/midasmovers/template.php
@@ -1,6 +1,6 @@
 <?php
-
-/**
- * @file
- * template.php
- */
+function midasmovers_preprocess_page(&$variables) {
+  unset($variables['navbar_classes_array'][
+    in_array('container', $variables['navbar_classes_array'])
+  ]);
+}
